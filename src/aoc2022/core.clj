19a74ae66b3c64ffc8be80aa1b8ca5edaa48c032
dokutan <54861821+dokutan@users.noreply.github.com>
2022-12-06 11:37:50 +0100

    day 06

--- src/aoc2022/core.clj
+++ src/aoc2022/core.clj
@@ -1,7 +1,7 @@
 (ns aoc2022.core
   (:gen-class))
 
-(require '[aoc2022.day05 :as day05])
+(require '[aoc2022.day06 :as day06])
 
 (defn -main
   "I don't do a whole lot ... yet."
---
+++ src/aoc2022/day06.clj
@@ -0,0 +1,24 @@
+(ns aoc2022.day06
+  (:require [clojure.string :as string])
+  (:require [clojure.java.io :as io]))
+
+(defn first-distinct-n [n]
+  (fn [data]
+    (loop [data data counter n]
+      (let [packet (take n data)]
+        (if (apply distinct? packet)
+          counter
+          (recur (rest data) (inc counter)))))))
+
+(println "day 06")
+(let [data (->> "day06"
+                (io/resource)
+                (slurp)
+                (char-array)
+                (vec))]
+
+  ;; part 1: 1134
+  (println ((first-distinct-n 4) data))
+
+  ;; part 2: 2263
+  (println ((first-distinct-n 14) data)))
